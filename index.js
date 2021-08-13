ac2d6984225cc7ce981d96502e8809439a720e14
Younes Tahiri <you@example.com>
2021-08-13 15:08:21 -0400

    initial commit for getAllMoviesReleasedAtOrBeforeYear(). plan of approach by pseudo coding all steps.

--- index.js
+++ index.js
@@ -224,7 +224,14 @@ function filterByGenre(movies, genre) {
       }
     ];
  */
-function getAllMoviesReleasedAtOrBeforeYear() {}
+function getAllMoviesReleasedAtOrBeforeYear(movies, year) {
+  // 0. creat a movieRelease variable that equals to empty array.
+  // 1. loop through movies array
+  // 1. use slice and number to access onlt the year in released.
+  // 3. check if released` year is equal to or less than the given year
+  // 4. store movie object in movieRelease array
+  // 5. if  no movie found with matching or less then the inputed year return empty array  else return obj.
+}
 
 /**
  * getBiggestBoxOfficeMovie()
